e16d008638546fca36700ba9c5c35e84928bf20f
sandratjushka <72806906+sandratjushka@users.noreply.github.com>
2023-08-12 02:53:53 +0300

    Update index.ts

--- index.ts
+++ index.ts
@@ -1,14 +1,32 @@
 import express, { Express, Request, Response } from "express";
 import cors from "cors";
+import bodyParser from "body-parser";
+import stringsController from "./controllers/strings";
+import productsController from "./controllers/products";
+import productListController from "./controllers/productlist";
+import parcelListController from "./controllers/parcelmachines";
+import eleringController from "./controllers/elering"
+import paymentController from "./controllers/payment"
+import payment from "./controllers/payment";
 
 const app: Express = express();
+
+app.use(cors({
+  origin: ['http://localhost:3006']
+}));
+app.use(bodyParser.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server');
 });
 
-app.listen(4000,() => {
-  console.log(`[server]: Server is running at http://localhost:4000`);
-});
+app.use('/', stringsController);
+app.use('/', productsController);
+app.use('/', productListController);
+app.use('/', parcelListController);
+app.use('/', eleringController);
+app.use('/', paymentController);
 
-app.use(cors())
+app.listen(3000,() => {
+  console.log(`[server]: Server is running at http://localhost:3000`);
+});
